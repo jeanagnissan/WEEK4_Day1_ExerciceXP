9abb56f838fcdae59e75466fbc3e246a203f9448
jeanagnissan <jean@digital.ci>
2023-01-10 09:54:32 +0000

    update fil produit.component.ts

--- src/app/my-app/produit/produit.component.ts
+++ src/app/my-app/produit/produit.component.ts
@@ -7,16 +7,6 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProduitComponent implements OnInit {
 
-  // const car: { type: string, model: string, year: number } = {
-  //   type: "Toyota",
-  //   model: "Corolla",
-  //   year: 2009
-  // };
-
-  name: string = "Sansumg galaxie fold 9";
-  prix: number = 299;
-
-
   produit: { name: string, prix: number } = {
     name: "Sansumg galaxie fold 9",
     prix: 299
